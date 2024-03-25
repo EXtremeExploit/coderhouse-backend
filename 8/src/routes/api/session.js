4cb2b0a83806d7d9af8bf6d4790e10a3b8132cec
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2024-03-25 16:47:12 -0300

    Entrega 8: Cookies, Session & Storage

---
+++ 8/src/routes/api/session.js
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import { userModel } from "../../models/user.js";
+
+const sessionRouter = Router();
+
+sessionRouter.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email: email }).lean();
+        if (user && password == user.password) {
+            req.session.email = email;
+            if (user.rol == "Admin") {
+                req.session.admin = true
+            }
+            res.status(200).redirect("/");
+        } else {
+            res.status(401).send("Usuario o contraseña no validos");
+        }
+    } catch (e) {
+        res.status(500).send("Error al loguear usuario: " + e);
+    }
+})
+
+sessionRouter.post('/register', async (req, res) => {
+    try {
+        const { first_name, last_name, email, password, age } = req.body;
+        const findUser = await userModel.findOne({ email: email });
+        if (findUser) {
+            res.status(400).send("Ya existe un usuario con este mail");
+        } else {
+            const isAdmin = email == 'adminCoder@coder.com';
+            await userModel.create({ first_name, last_name, email, age, password, rol: isAdmin ? "Admin" : "User" });
+            res.status(200).send("Usuario creado correctamente");
+        }
+    } catch (e) {
+        res.status(500).send("Error al registrar users: " + e);
+    }
+
+
+})
+
+sessionRouter.get('/logout', (req, res) => {
+    req.session.destroy(function (e) {
+        if (e) {
+            console.log(e);
+        } else {
+            res.status(200).redirect("/");
+        }
+    })
+})
+
+export default sessionRouter;
