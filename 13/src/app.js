4a666b6e3447539ff3e13aa2caf7271eba0e0fc0
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2024-05-20 15:05:40 -0300

    Entrega 13: Optimizacion y Mocking

---
+++ 13/src/app.js
@@ -0,0 +1,140 @@
+import config from './config.js'
+import express from 'express';
+import { engine } from 'express-handlebars';
+import mongoose from 'mongoose';
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+import { Server } from 'socket.io';
+import cookieParser from 'cookie-parser';
+
+import apiRouter from './routes/api.js';
+import productModel from './models/products.js';
+import messageModel from './models/messages.js';
+import initializePassport from './config/passport/passport.js';
+import passport from 'passport';
+import { generateProduct } from './controllers/mock.js';
+
+const app = express();
+
+mongoose.connect(config.mongo_url).then(() => console.log('DB Connected')).catch((e) => console.log(e));
+
+app.use(express.json());
+
+app.use(session({
+    secret: config.session_secret,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: config.mongo_url,
+        ttl: 60 * 60
+    }),
+}));
+
+app.use(cookieParser(config.cookies_secret));
+
+initializePassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api', apiRouter);
+
+const server = app.listen(config.port, () => {
+    console.log('Listening on port: ' + config.port);
+});
+
+const io = new Server(server);
+
+app.use('/public', express.static('src/public'));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', 'src/views');
+
+io.on('connection', (socket) => {
+    console.log('cliente conectado');
+    // Realtime Products
+    socket.on('pullProducts', async (info) => {
+        socket.emit('products', await productModel.find().lean());
+    });
+
+    // Mensajes
+    socket.on('mensaje', async (msg) => {
+        try {
+            await messageModel.create(msg);
+            const mensajes = await messageModel.find().lean();
+            io.emit('mensajeLogs', mensajes);
+        } catch (e) {
+            io.emit('mensajeLogs', e);
+        }
+    })
+});
+
+app.get('/', async (req, res) => {
+    const prods = await productModel.find().lean();
+
+    res.render('templates/index', {
+        mostrarProductos: true,
+        productos: prods,
+        js: 'index',
+        email: req.user?.email,
+        rol: req.user?.rol,
+        loggedIn: !!req.user?.email
+    });
+});
+
+app.get('/chat', async (req, res) => {
+    res.render("templates/chat", {
+        js: 'chat',
+    });
+});
+
+
+app.get('/realTimeProducts', async (req, res) => {
+    res.render('templates/realTimeProducts', {
+        js: 'realTimeProducts'
+    });
+});
+
+app.get('/register', async (req, res) => {
+    res.render('templates/register', {
+        js: 'register'
+    });
+});
+
+
+app.get('/login', async (req, res) => {
+    res.render('templates/login', {
+        js: 'login'
+    });
+});
+
+// Mock
+app.get('/mockingproducts', async (req, res) => {
+    const PRODS_SIZE = 100;
+    try {
+        let mockProducts = [];
+
+        for (let i = 0; i < PRODS_SIZE; i++) {
+            let element = generateProduct();
+            mockProducts.push(element);
+        }
+
+        res.status(200).send(mockProducts);
+
+    } catch (error) {
+        res.status(500).send(`Error al generar ${PRODS_SIZE} productos: ${e}`);
+    }
+})
+
+
+//Session Routes
+
+app.get('/session', (req, res) => {
+    if (req.session.counter) {
+        req.session.counter++;
+        res.send(`Sos el usuario N° ${req.session.counter} en ingresar a la pagina`);
+    } else {
+        req.session.counter = 1;
+        res.send("Sos el primer usuario que ingresa a la pagina");
+    }
+});
---
+++ 13/src/controllers/cart.js
@@ -0,0 +1,102 @@
+import crypto from 'crypto';
+import cartModel from '../models/cart.js';
+import productModel from '../models/products.js';
+import ticketModel from '../models/ticket.js';
+
+export async function getCart(id) {
+    const cart = await cartModel.findOne({ _id: id });
+    return cart;
+}
+
+export async function deleteCart(id) {
+    const msg = await cartModel.findByIdAndUpdate(id, { products: [] });
+    return msg;
+}
+
+export async function updateCart(id, body) {
+    const msg = await cartModel.findByIdAndUpdate(id, { products: body });
+    return msg;
+}
+
+export async function addProduct(cid, pid, quantity) {
+    const cart = await cartModel.findById(cid);
+
+    const i = cart.products.findIndex((p) => p.id_prod == pid);
+
+    if (i != -1) {
+        cart.products[i].quantity += (quantity ?? 1);
+    } else {
+        cart.products.push({ id_prod: pid, quantity: quantity });
+    }
+
+    const msg = await cartModel.findByIdAndUpdate(cid, cart);
+    return msg;
+}
+
+export async function setProduct(cid, pid, quantity) {
+
+    const cart = await cartModel.findById(cid);
+
+    const i = cart.products.findIndex((p) => p.id_prod == pid);
+
+    if (i != -1) {
+        cart.products[i].quantity = quantity;
+    }
+
+    const msg = await cartModel.findByIdAndUpdate(cid, cart);
+    return msg;
+}
+
+export async function deleteProduct(cid, pid) {
+
+    const cart = await cartModel.findById(cid);
+
+    const i = cart.products.findIndex((p) => p.id_prod == pid);
+
+    if (i != -1) {
+        cart.products.splice(i, 1);
+    }
+
+    const msg = await cartModel.findByIdAndUpdate(cid, cart);
+    return msg;
+}
+
+export async function purchase(cid, email) {
+    const cart = await cartModel.findById(cid)
+    const prodSinStock = []
+    if (cart) {
+        cart.products.filter(async (e) => {
+            const producto = await productModel.findById(cartProd.id_prod);
+            if (producto.stock < cartProd.quantity) {
+                prodSinStock.push({ producto, stockCarrito: cartProd.quantity });
+                return false;
+            }
+            return true;
+        });
+        let amount = 0;
+        cart.products.forEach(async (cartProd) => {
+            let producto = await productModel.findById(cartProd.id_prod);
+            amount += producto.price;
+            producto.stock--;
+            await productModel.findByIdAndUpdate(producto._id, producto);
+        });
+
+        cart.products = prodSinStock.map((e) => e.producto);
+        await cartModel.findByIdAndUpdate(cid, cart);
+
+        const ticket = await ticketModel.create({
+            products: cart.products,
+            amount: amount,
+            purchase_datetime: Date.now(),
+            purchaser: email,
+            code: crypto.randomBytes(8).toString('hex')
+        });
+
+        if (prodSinStock.length == 0)
+            return `Compra realizada con exito. Su ticket es ${ticket.code}`;
+        return `Error al comprar los siguientes productos por falta de stock: \n${prodSinStock.map((e) => `${e.producto.title} | Stock en el carrito: ${e.stockCarrito} | Stock disponible: ${e.producto.stock}\n`)}\n El resto de productos han sido comprados.`;
+
+    } else {
+        res.status(404).send("Carrito no existe")
+    }
+}
